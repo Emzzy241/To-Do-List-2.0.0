1d588c2bfcf1b8f6f74e8aa23546fc412203951c
Emzzy241 <emzzyoluwole@gmail.com>
2023-12-19 14:28:08 +0100

    add code file

---
+++ Todolist/Program.cs
@@ -0,0 +1,29 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ToDoList
+{
+  class Program
+  {
+    public static void Main(string[] args)
+    {
+      WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+      builder.Services.AddControllersWithViews();
+
+      WebApplication app = builder.Build();
+
+      app.UseHttpsRedirection();
+
+      app.UseRouting();
+
+      app.MapControllerRoute(
+        name: "default",
+        pattern: "{controller=Home}/{action=Index}/{id?}"
+      );
+
+      app.Run();
+
+    }
+  }
+}
