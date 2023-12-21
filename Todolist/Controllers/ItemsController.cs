d6c6c3946ad5c7a24fbf1870730c56385fb32958
Emzzy241 <emzzyoluwole@gmail.com>
2023-12-21 17:39:52 +0100

    made new changes to app

--- Todolist/Controllers/ItemsController.cs
+++ Todolist/Controllers/ItemsController.cs
@@ -14,17 +14,27 @@ namespace ToDoList.Controllers
             return View(myItemList);
         }
 
+
+
         [HttpGet("/items/new")]
-        public ActionResult CreateForm()
+        public ActionResult New()
         {
-        return View();
+            return View();
         }
+
 
         [HttpPost("/items")]
         public ActionResult Create(string description)
         {
         Item myItem = new Item(description);
         return RedirectToAction("Index");
+        }
+        
+        [HttpGet("/items/{id}")]
+        public ActionResult Show(int id)
+        {
+            Item foundItem = Item.Find(id);
+            return View(foundItem);
         }
 
         [HttpGet("/items/delete")]
--- Todolist/Models/Item.cs
+++ Todolist/Models/Item.cs
@@ -7,6 +7,9 @@ namespace ToDoList.Models
     {
         public string Description { get; set; }
 
+        // We don't add a set method, because this property will be set in the constructor automatically. In fact, we specifically don't ever want to manually edit it. That would increase the risk of IDs not being unique
+        public int Id { get; }
+
         private static List<Item> _myList = new List<Item>() {};
         
         
@@ -14,6 +17,8 @@ namespace ToDoList.Models
         {
             Description = myDescription;
             _myList.Add(this);
+            // This assigns an Item's Id to the current number of Items in the static _instances list. After the first Item is added, _instances.Count will be 1. After the second Item, it'll be 2, and so on. Using _instances.Count to assign Id ensures each is always unique. Note that we do this after adding Items to the _instances list in order to get an updated Count for Id.
+            Id = _myList.Count;
         }
 
         public static List<Item> GetAll() 
@@ -25,7 +30,10 @@ namespace ToDoList.Models
         { 
             _myList.Clear(); 
         }
-        
-        
+
+        public static Item Find(int searchId)
+        {
+            return _myList[searchId - 1];
+        }    
     }
 }
